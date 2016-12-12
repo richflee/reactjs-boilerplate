7162a621fb1004693c1e08a8800ceb2ee1780d31
Richard Lee <rich@Richards-MacBook-Pro.local>
2016-12-12 12:55:31 +1100

    Adding all base boilerplate files.

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <p>Boilerplate3 Project</p>,
+document.getElementById('app'));
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+// create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forward-proto'] === 'https'){
+    res.redirect('http://' + req.hostname + req.url);
+  }
+  else {
+    next();
+  }
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
+});
