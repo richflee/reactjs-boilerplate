a00301c1f9a321068253c533c6b707339df4f1db
Richard Lee <richard.lee@carsales.com.au>
2018-11-01 22:02:56 +1100

    Removed redundant code.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-// var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
-// // load foundation
-// $(document).foundation();
-
-// // App css
-// require('style!css!sass!applicationStyles')
-
-// ReactDOM.render(
-//   <p>Boilerplate3 Project</p>,
-// document.getElementById('app'));
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./js/components/containers/App";
